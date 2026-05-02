285872b53a3d9783b7b1a8f66666a27767f2c4aa
Hi, I am Saeed Zafar <saeedzafar4595@gmail.com>
2026-05-03 03:13:07 +0500

    Refactor auth.controller.ts for clarity

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { Response } from 'express';
-
+//auth contrller
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
